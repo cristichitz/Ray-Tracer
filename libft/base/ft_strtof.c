b5a8adba294cfd678d69b5666613d744442d1a7c
Tiisetso Murray <timurray@student.hive.fi>
2026-02-09 12:42:28 +0200

    refactor: strtof split functions.

--- libft/base/ft_strtof.c
+++ libft/base/ft_strtof.c
@@ -6,71 +6,89 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 11:09:41 by timurray          #+#    #+#             */
-/*   Updated: 2026/02/08 16:33:30 by timurray         ###   ########.fr       */
+/*   Updated: 2026/02/09 12:41:58 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-float	ft_strtof(const char *nptr, char **endptr)
+static inline int	handle_sign(const char **nptr)
 {
-	unsigned int	i;
-	int				sign;
-	int				e_sign;
-	double			num;
-	double			scale;
-	int				e;
+	int	sign;
 
-	i = 0;
-	while (ft_isspace(nptr[i]))
-		i++;
 	sign = 1;
-	if (nptr[i] == '+' || nptr[i] == '-')
+	if (**nptr == '+' || **nptr == '-')
 	{
-		if (nptr[i] == '-')
+		if (**nptr == '-')
 			sign = -1;
-		i++;
+		(*nptr)++;
 	}
-	num = 0.0;
-	while (ft_isdigit(nptr[i]))
+	return (sign);
+}
+
+static inline void	handle_scale(const char **nptr, double *num)
+{
+	double	scale;
+
+	scale = 1.0;
+	if (**nptr == '.')
 	{
-		num = num * 10.0 + (nptr[i] - '0');
-		i++;
-	}
-	if (nptr[i] == '.')
-	{
-		i++;
-		while (ft_isdigit(nptr[i]))
+		(*nptr)++;
+		while (ft_isdigit(**nptr))
 		{
-			num = num * 10.0 + (nptr[i] - '0');
+			*num = *num * 10.0 + (**nptr - '0');
 			scale *= 10.0;
-			i++;
+			(*nptr)++;
 		}
 	}
-	num = num / scale;
-	if (nptr[i] == 'e' || nptr == 'E')
+	*num /= scale;
+}
+
+static inline void	handle_exp(const char **nptr, double *num)
+{
+	int	e_sign;
+	int	e;
+
+	e_sign = 1;
+	e = 0;
+	if (**nptr == 'e' || **nptr == 'E')
 	{
-		i++;
-		if (nptr[i] == '+' || nptr[i] == '-')
+		(*nptr)++;
+		e_sign = handle_sign(nptr);
+		while (ft_isdigit(**nptr))
 		{
-			if (nptr[i] == '-')
-				e_sign = -1;
-			i++;
-		}
-		while (ft_isdigit(nptr[i]))
-		{
-			e = e * 10 + (nptr[i] - '0');
-			i++;
+			e = e * 10 + (**nptr - '0');
+			(*nptr)++;
 		}
 	}
 	while (e-- > 0)
 	{
 		if (e_sign == 1)
-			num *= 10.0;
+			*num *= 10.0;
 		else
-			num /= 10.0;
+			*num /= 10.0;
 	}
+}
+
+float	ft_strtof(const char *nptr, char **endptr)
+{
+	int		sign;
+	int		e_sign;
+	double	num;
+	int		e;
+
+	while (ft_isspace(*nptr))
+		nptr++;
+	sign = handle_sign(&nptr);
+	num = 0.0;
+	while (ft_isdigit(*nptr))
+	{
+		num = num * 10.0 + (*nptr - '0');
+		nptr++;
+	}
+	handle_scale(&nptr, &num);
+	handle_exp(&nptr, &num);
 	if (endptr)
-		*endptr = nptr[i];
+		*endptr = (char *)nptr;
 	return (float)(sign * num);
 }
