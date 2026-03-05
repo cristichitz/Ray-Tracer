4842b0bf9d26dc9b2c01e8563d8888fef80ab654
Tiisetso Murray <timurray@student.hive.fi>
2026-03-05 12:57:33 +0200

    style: quick format in the src/class.

--- src/class/sphere.c
+++ src/class/sphere.c
@@ -1,51 +1,52 @@
 #include "sphere.h"
 
-bool  hit_sphere(void *base, t_ray ray, t_interval t, t_hit_record *rec)
+bool	hit_sphere(void *base, t_ray ray, t_interval t, t_hit_record *rec)
 {
-  t_sphere *self;
+	t_sphere	*self;
+	t_vec3		oc;
+	float		a;
+	float		h;
+	float		c;
+	float		discriminant;
+	float		sqrtd;
+	float		root;
+	t_vec3		outward_normal;
 
-  self = (t_sphere *)base;
-  t_vec3 oc = sub(self->center, ray.origin);
-
-  float a = dot(ray.dir, ray.dir);
-  float h = dot(ray.dir, oc);
-  float c = dot(oc, oc) - self->radius * self->radius;
-
-  float discriminant = h*h - a * c;
-  if (discriminant < 0)
-    return (false);
-
-  float sqrtd = sqrt(discriminant);
-
-  float root = (h - sqrtd) / a;
-  if (!t.surrounds(&t, root))
-  {
-    root = (h + sqrtd) / a;
-    if (!t.surrounds(&t, root))
-      return (false);
-  }
-
-  rec->t = root;
-  //point at t in the ray direction
-  rec->p = ray.at(&ray, rec->t);
-
-  // Point - sphere_center is already a normal vector
-  // Dividing by the radius makes it unit length aswell
-  t_vec3 outward_normal = divide(sub(rec->p, self->center), self->radius);
-  rec->set_face_normal(rec, ray, outward_normal);
-  
-  return (true);
+	self = (t_sphere *)base;
+	oc = sub(self->center, ray.origin);
+	a = dot(ray.dir, ray.dir);
+	h = dot(ray.dir, oc);
+	c = dot(oc, oc) - self->radius * self->radius;
+	discriminant = h * h - a * c;
+	if (discriminant < 0)
+		return (false);
+	sqrtd = sqrt(discriminant);
+	root = (h - sqrtd) / a;
+	if (!t.surrounds(&t, root))
+	{
+		root = (h + sqrtd) / a;
+		if (!t.surrounds(&t, root))
+			return (false);
+	}
+	rec->t = root;
+	// point at t in the ray direction
+	rec->p = ray.at(&ray, rec->t);
+	// Point - sphere_center is already a normal vector
+	// Dividing by the radius makes it unit length aswell
+	outward_normal = divide(sub(rec->p, self->center), self->radius);
+	rec->set_face_normal(rec, ray, outward_normal);
+	return (true);
 }
 
-t_sphere* make_sphere(t_vec3 center, float radius)
+t_sphere	*make_sphere(t_vec3 center, float radius)
 {
-  t_sphere *s;
+	t_sphere	*s;
 
-  s = malloc(sizeof(t_sphere));
-  if (!s)
-    return (NULL);
-  s->radius = radius;
-  s->center = center;
-  s->base.hit = hit_sphere;
-  return (s);
+	s = malloc(sizeof(t_sphere));
+	if (!s)
+		return (NULL);
+	s->radius = radius;
+	s->center = center;
+	s->base.hit = hit_sphere;
+	return (s);
 }
--- src/class/vec3.c
+++ src/class/vec3.c
@@ -1,42 +1,44 @@
 #include "vec3.h"
 
-t_vec3 make_vec(float a, float b, float c)
+t_vec3	make_vec(float a, float b, float c)
 {
-  return (t_vec3){a, b, c};
+	return ((t_vec3){a, b, c});
 }
 
-t_vec3 add(t_vec3 a, t_vec3 b)
+t_vec3	add(t_vec3 a, t_vec3 b)
 {
-  return (t_vec3){a.x + b.x, a.y + b.y, a.z + b.z};
+	return ((t_vec3){a.x + b.x, a.y + b.y, a.z + b.z});
 }
 
-t_vec3 sub(t_vec3 a, t_vec3 b)
+t_vec3	sub(t_vec3 a, t_vec3 b)
 {
-  return (t_vec3){a.x - b.x, a.y - b.y, a.z - b.z};
+	return ((t_vec3){a.x - b.x, a.y - b.y, a.z - b.z});
 }
 
-t_vec3 scale(t_vec3 a, float b)
+t_vec3	scale(t_vec3 a, float b)
 {
-  return (t_vec3){a.x * b, a.y * b, a.z * b};
+	return ((t_vec3){a.x * b, a.y * b, a.z * b});
 }
 
-t_vec3 divide(t_vec3 a, float b)
+t_vec3	divide(t_vec3 a, float b)
 {
-  return (t_vec3){a.x / b, a.y / b, a.z /b};
+	return ((t_vec3){a.x / b, a.y / b, a.z / b});
 }
 
-float dot(t_vec3 a, t_vec3 b)
+float	dot(t_vec3 a, t_vec3 b)
 {
-  return (a.x * b.x + a.y * b.y + a.z * b.z);
+	return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
-t_vec3 norm(t_vec3 a)
+t_vec3	norm(t_vec3 a)
 {
-  float len = sqrtf(a.x *a.x + a.y * a.y + a.z * a.z);
-  return make_vec(a.x / len, a.y / len, a.z / len);
+	float	len;
+
+	len = sqrtf(a.x * a.x + a.y * a.y + a.z * a.z);
+	return (make_vec(a.x / len, a.y / len, a.z / len));
 }
 
-void  print_vec(t_vec3 a)
+void	print_vec(t_vec3 a)
 {
-  printf("(%f, %f, %f)\n", a.x, a.y, a.z);
+	printf("(%f, %f, %f)\n", a.x, a.y, a.z);
 }
