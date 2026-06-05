58a53c2b3d1400fcb4f198d80c34356afd20cd57
Tiisetso Daniel Murray <tiisetsomurray@gmail.com>
2026-06-06 00:01:41 +0300

    wip: resize object

--- src/resize.c
+++ src/resize.c
@@ -13,24 +13,17 @@
 #include "MLX42/MLX42.h"
 #include "rt_cpu.h"
 
-void	resize_object(t_data *data, float *scalar)
-{
-	t_hittable	*object;
+void resize_object(t_data *data, float *scalar) {
+  t_hittable *object;
 
-	if (data->world.objects->len > 0)
-	{
-		object = (t_hittable *)ft_vec_get(data->world.objects, data->object_i
-				% data->world.objects->len);
-		object->resize(object, *scalar);
-		if (object->resize == NULL)
-		{
-			if (object->resize)
-			{
-				if (mlx_is_key_down(data->mlx, MLX_KEY_KP_ADD))
-					object->resize(object, *scalar);
-				if (mlx_is_key_down(data->mlx, MLX_KEY_KP_SUBTRACT))
-					object->resize(object, -(*scalar));
-			}
-		}
-	}
+  if (data->world.objects->len > 0) {
+    object = (t_hittable *)ft_vec_get(
+        data->world.objects, data->object_i % data->world.objects->len);
+    if (object->resize) {
+      if (mlx_is_key_down(data->mlx, MLX_KEY_K))
+        object->resize(object, *scalar);
+      if (mlx_is_key_down(data->mlx, MLX_KEY_L))
+        object->resize(object, -(*scalar));
+    }
+  }
 }
