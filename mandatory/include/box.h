7aedfb0b6ac64df525ca6a68a517f27ddc299f79
Tiisetso Murray <timurray@student.hive.fi>
2026-06-13 13:02:02 +0300

    fix: box.h guard

--- mandatory/include/box.h
+++ mandatory/include/box.h
@@ -1,14 +1,28 @@
-#ifndef PLANE_H
-# define PLANE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   box.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/13 13:00:40 by timurray          #+#    #+#             */
+/*   Updated: 2026/06/13 13:00:47 by timurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BOX_H
+# define BOX_H
 
 # include "hittable.h"
-# include "quad.h"
 # include "plane.h"
-# include "stdlib.h"
-typedef	struct s_box {
+# include "quad.h"
+# include <stdlib.h>
+
+typedef struct s_box
+{
 	t_hittable	base;
 	t_quad		*sides[6];
-} t_box;
+}				t_box;
 
 t_box			*make_box(t_vec3 a, t_vec3 b, t_material mat);
 
