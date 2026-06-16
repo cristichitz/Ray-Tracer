88b4b03bb5154e62861995c702bd32630312cab6
Tiisetso Murray <timurray@student.hive.fi>
2026-06-16 17:52:36 +0300

    fix: control adjustment.

--- mandatory/src/control/move_object.c
+++ mandatory/src/control/move_object.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   move_object.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdohanic <cdohanic@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 17:35:02 by cdohanic          #+#    #+#             */
-/*   Updated: 2026/06/13 17:35:05 by cdohanic         ###   ########.fr       */
+/*   Updated: 2026/06/16 15:36:11 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,9 @@ static t_vec3	move_step(t_data *data, float *speed)
 	if (key_down2(data, MLX_KEY_KP_4, MLX_KEY_F))
 		step = add(step, scale(right, -*speed));
 	if (key_down2(data, MLX_KEY_KP_5, MLX_KEY_G))
-		step = add(step, scale(data->cam_forward, *speed));
-	if (key_down2(data, MLX_KEY_KP_8, MLX_KEY_T))
 		step = add(step, scale(data->cam_forward, -*speed));
+	if (key_down2(data, MLX_KEY_KP_8, MLX_KEY_T))
+		step = add(step, scale(data->cam_forward, *speed));
 	if (key_down2(data, MLX_KEY_KP_9, MLX_KEY_Y))
 		step = add(step, scale(up, *speed));
 	if (key_down2(data, MLX_KEY_KP_7, MLX_KEY_R))
